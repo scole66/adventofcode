d70cf64e61191a1d380246bfefedbb62601f0d57
Steven Cole <scole66@gmail.com>
2022-12-02 16:23:53 -0800

    wip

--- 2021/day19/src/main.rs
+++ 2021/day19/src/main.rs
@@ -39,6 +39,16 @@ impl Scanner {
     fn new(name: &str) -> Self {
         Scanner { id: name.to_string(), beacons: AHashSet::new() }
     }
+
+    fn distances(&self) -> Vec<f64> {
+        for head in self.beacons.iter() {
+            for tail in 
+        }
+    }
+
+    fn shared_beacons(&self, other: &Scanner) -> AHashSet<Coords> {
+        
+    }
 }
 
 /// A NewType wrapping an `anyhow::Result<String>`
@@ -307,5 +317,91 @@ mod variations {
         assert!(output.contains(&vec![0, 1, 0, -1, 0, 0, 0, 0, -1]));
         assert!(output.contains(&vec![0, -1, 0, 1, 0, 0, 0, 0, -1]));
         assert!(output.contains(&vec![0, -1, 0, -1, 0, 0, 0, 0, 1]));
+    }
+}
+
+mod intersection {
+    use super::*;
+    use indoc::indoc;
+    #[test]
+    fn from_problem_statement() {
+        let input = indoc! {"
+            --- scanner 0 ---
+            404,-588,-901
+            528,-643,409
+            -838,591,734
+            390,-675,-793
+            -537,-823,-458
+            -485,-357,347
+            -345,-311,381
+            -661,-816,-575
+            -876,649,763
+            -618,-824,-621
+            553,345,-567
+            474,580,667
+            -447,-329,318
+            -584,868,-557
+            544,-627,-890
+            564,392,-477
+            455,729,728
+            -892,524,684
+            -689,845,-530
+            423,-701,434
+            7,-33,-71
+            630,319,-379
+            443,580,662
+            -789,900,-551
+            459,-707,401
+            
+            --- scanner 1 ---
+            686,422,578
+            605,423,415
+            515,917,-361
+            -336,658,858
+            95,138,22
+            -476,619,847
+            -340,-569,-846
+            567,-361,727
+            -460,603,-452
+            669,-402,600
+            729,430,532
+            -500,-761,534
+            -322,571,750
+            -466,-666,-811
+            -429,-592,574
+            -355,545,-477
+            703,-491,-529
+            -328,-685,520
+            413,935,-424
+            -391,539,-444
+            586,-435,557
+            -364,-763,-893
+            807,-499,-711
+            755,-354,-619
+            553,889,-390
+        "}
+        .lines()
+        .map(LineResult::from)
+        .collect::<anyhow::Result<Vec<Scanner>>>()
+        .unwrap();
+
+        let result = input[0].shared_beacons(&input[1]);
+        let expected = vec![
+            Coords(-618, -824, -621),
+            Coords(-537, -823, -458),
+            Coords(-447, -329, 318),
+            Coords(404, -588, -901),
+            Coords(544, -627, -890),
+            Coords(528, -643, 409),
+            Coords(-661, -816, -575),
+            Coords(390, -675, -793),
+            Coords(423, -701, 434),
+            Coords(-345, -311, 381),
+            Coords(459, -707, 401),
+            Coords(-485, -357, 347),
+        ];
+
+        assert_eq!(result.len(), expected.len());
+        assert!(expected.iter().all(|coord| result.contains(coord)));
     }
 }
