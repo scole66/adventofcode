23a80658fedb86674b96dab3d5353661eb22e197
Steven Cole <scole66@gmail.com>
2022-12-28 17:56:24 -0800

    Update the template

--- template.rs
+++ template.rs
@@ -1,23 +1,22 @@
-//! # Solution for Advent of Code 2022 Day XXX: 
+//! # Solution for Advent of Code 2022 Day XXX:
 //!
 //! Ref: [Advent of Code 2022 Day XXX](https://adventofcode.com/2022/day/XXX)
 //!
-#![allow(dead_code,unused_imports,unused_variables)]
-use ahash::AHashSet;
-use anyhow::Context;
+#![allow(dead_code, unused_imports, unused_variables)]
+use ahash::{AHashMap, AHashSet};
+use anyhow::{anyhow, bail, Context, Error, Result};
 use std::io::{self, Read};
-use std::iter::Iterator;
 use std::str::FromStr;
 
-fn part1(input: &str) -> anyhow::Result<usize> {
+fn part1(input: &str) -> Result<usize> {
     todo!()
 }
 
-fn part2(input: &str) -> anyhow::Result<usize> {
+fn part2(input: &str) -> Result<usize> {
     todo!()
 }
 
-fn main() -> anyhow::Result<()> {
+fn main() -> Result<()> {
     let stdin = io::stdin();
 
     let mut input = String::new();
