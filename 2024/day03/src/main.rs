a9fcae8bf82f1c804c0bb0a7e1614715beecff44
Steven Cole <scole66@gmail.com>
2024-12-02 11:50:45 -0800

    feat: AoC 2024 Day 3

---
+++ 2024/day03/src/main.rs
@@ -0,0 +1,58 @@
+//! # Solution for Advent of Code 2024 Day 3:
+//!
+//! Ref: [Advent of Code 2024 Day 3](https://adventofcode.com/2024/day/3)
+//!
+#![allow(dead_code, unused_imports, unused_variables)]
+use ahash::{AHashMap, AHashSet};
+use anyhow::{anyhow, bail, Context, Error, Result};
+use std::io::{self, Read};
+use std::str::FromStr;
+
+
+struct Input {}
+impl FromStr for Input {
+    type Err = Error;
+    fn from_str(s: &str) -> Result<Self> {
+        todo!()
+    }
+}
+
+fn part1(input: &Input) -> Result<usize> {
+    todo!()
+}
+
+fn part2(input: &Input) -> Result<usize> {
+    todo!()
+}
+
+fn main() -> Result<()> {
+    let stdin = io::stdin();
+
+    let mut input = String::new();
+    stdin.lock().read_to_string(&mut input)?;
+    let input = input.parse::<Input>()?;
+
+    println!("Part1: {}", part1(&input)?);
+    println!("Part2: {}", part2(&input)?);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static SAMPLE: &str = indoc::indoc! {"
+    "};
+
+    #[test]
+    fn part1_sample() {
+        assert_eq!(part1(&SAMPLE.parse::<Input>().unwrap()).unwrap(), 13);
+    }
+
+    #[test]
+    #[should_panic]
+    fn part2_sample() {
+        assert_eq!(part2(&SAMPLE.parse::<Input>().unwrap()).unwrap(), 36);
+    }
+}
