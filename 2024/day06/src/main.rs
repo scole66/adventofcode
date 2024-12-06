ce28dce87b8c245b6cd4043f4bd5e265f74a8991
Steven Cole <scole66@gmail.com>
2024-12-06 08:50:40 -0800

    Turn off the link ignores

--- 2024/day06/src/main.rs
+++ 2024/day06/src/main.rs
@@ -2,9 +2,8 @@
 //!
 //! Ref: [Advent of Code 2024 Day 6](https://adventofcode.com/2024/day/6)
 //!
-#![allow(dead_code, unused_imports, unused_variables)]
 use ahash::{AHashMap, AHashSet};
-use anyhow::{anyhow, bail, Context, Error, Result};
+use anyhow::{anyhow, bail, Error, Result};
 use std::io::{self, Read};
 use std::str::FromStr;
 
