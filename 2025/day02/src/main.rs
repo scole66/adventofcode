eb066bc96ee19c8993eaa0a44eda7a409a36b1ca
Steven Cole <scole66@gmail.com>
2025-12-01 22:12:37 -0800

    Advent of Code 2025 Day 2 (#182)

---
+++ 2025/day02/src/main.rs
@@ -0,0 +1,156 @@
+//! # Solution for Advent of Code 2025 Day 2: Gift Shop
+//!
+//! Ref: [Advent of Code 2025 Day 2](https://adventofcode.com/2025/day/2)
+//!
+use anyhow::{Context, Error, Result, anyhow};
+use std::io::{self, Read};
+use std::str::FromStr;
+
+struct Pair {
+    start: i64,
+    end: i64,
+}
+impl FromStr for Pair {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        let (start_str, end_str) = s.split_once('-').ok_or(anyhow!("Invalid pair: {s}"))?;
+        let start = start_str
+            .parse::<i64>()
+            .context(format!("Parsing {start_str} as a start value"))?;
+        let end = end_str
+            .trim()
+            .parse::<i64>()
+            .context(format!("Parsing {end_str} as an end value"))?;
+        Ok(Pair { start, end })
+    }
+}
+struct Input {
+    pairs: Vec<Pair>,
+}
+impl FromStr for Input {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        Ok(Input {
+            pairs: s
+                .split(',')
+                .map(|line| line.parse::<Pair>())
+                .collect::<Result<Vec<_>>>()?,
+        })
+    }
+}
+
+fn divisors(n: usize) -> Vec<usize> {
+    let mut divs = Vec::new();
+    for i in 1..=n {
+        if n.is_multiple_of(i) {
+            divs.push(i);
+        }
+    }
+    divs
+}
+
+impl Pair {
+    fn invalid_ids(&self) -> Vec<i64> {
+        let mut invalids = Vec::new();
+        for id in self.start..=self.end {
+            let id_str = id.to_string();
+            let num_digits = id_str.len();
+            if !id_str.is_empty() && num_digits % 2 == 0 {
+                let (left, right) = id_str.split_at(num_digits / 2);
+                if left == right {
+                    invalids.push(id);
+                }
+            }
+        }
+        invalids
+    }
+
+    fn truly_invalid_ids(&self) -> Vec<i64> {
+        let mut invalids = Vec::new();
+        for id in self.start..=self.end {
+            let id_str = id.to_string();
+            let num_digits = id_str.len();
+            for window_size in divisors(num_digits) {
+                if window_size == num_digits {
+                    continue;
+                }
+                let mut all_match = true;
+                for start in (0..num_digits).step_by(window_size) {
+                    let end = start + window_size;
+                    let slice = &id_str[start..end];
+                    let next_start = end;
+                    let next_end = next_start + window_size;
+                    if next_end > num_digits {
+                        continue;
+                    }
+                    if &id_str[next_start..next_end] != slice {
+                        all_match = false;
+                        break;
+                    }
+                }
+
+                if all_match {
+                    invalids.push(id);
+                    break;
+                }
+            }
+        }
+        invalids
+    }
+}
+
+fn part1(input: &Input) -> i64 {
+    input
+        .pairs
+        .iter()
+        .map(|pair| pair.invalid_ids().iter().sum::<i64>())
+        .sum()
+}
+
+fn part2(input: &Input) -> i64 {
+    input
+        .pairs
+        .iter()
+        .map(|pair| pair.truly_invalid_ids().iter().sum::<i64>())
+        .sum()
+}
+
+fn main() -> Result<()> {
+    let stdin = io::stdin();
+
+    let mut input = String::new();
+    stdin.lock().read_to_string(&mut input)?;
+    let input = input.parse::<Input>()?;
+
+    let start_time = std::time::Instant::now();
+    let part1 = part1(&input);
+    let part2 = part2(&input);
+    let elapsed = start_time.elapsed();
+
+    println!("Part1: {part1}");
+    println!("Part2: {part2}");
+    println!("Time: {elapsed:?}");
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static SAMPLE: &str = indoc::indoc! {"
+        11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
+    "};
+
+    #[test]
+    fn part1_sample() {
+        assert_eq!(part1(&SAMPLE.parse::<Input>().unwrap()), 1227775554);
+    }
+
+    #[test]
+    fn part2_sample() {
+        assert_eq!(part2(&SAMPLE.parse::<Input>().unwrap()), 4174379265);
+    }
+}
